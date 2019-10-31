b46fab640f7bf818e4475b9f24c357029e636944
bree <dwf1023@163.com>
2019-10-31 10:32:42 +0800

    12

---
+++ MpGenerator/src/main/java/com/bree/MpGenerator/MpGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.bree.MpGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpGeneratorApplication.class, args);
+	}
+
+}
---
+++ MpGenerator/src/test/java/com/bree/MpGenerator/MpGeneratorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bree.MpGenerator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MpGeneratorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
